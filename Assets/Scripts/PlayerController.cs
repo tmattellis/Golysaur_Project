121bebd1261fd31c467e1bf538bf2e1099573f6b
tmattellis <t.ellis897@gmail.com>
2020-02-27 20:35:11 -0600

    adding changes after merging manually
    (some things are broken, which are listed on the Golysaur Roles spreadsheet)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,7 +10,9 @@ public class PlayerController : MonoBehaviour
 
     public double highJumpTimer;
     public double speedTimer;
-    public double projectileTimer;
+    public int projectileNum;
+    public int startProjectileNum;
+    public int projectileIncrement;
 
     public GameObject projectilePrefab;
 
@@ -17,93 +20,145 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        projectileNum = startProjectileNum;
     }
 
     // Update is called once per frame
     void Update()
     {
         // move player left
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            if(speedTimer > 0)
+            if (speedTimer > 0)
             {
-                rigidbody.AddForce(Vector2.left *25f);
+                rigidbody.AddForce(Vector2.left * 25f);
             }
             else
             {
-                rigidbody.AddForce(Vector2.left *14f);
+                rigidbody.AddForce(Vector2.left * 14f);
             }
         }
 
         // move player right
-        if(Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            if(speedTimer > 0)
+            if (speedTimer > 0)
             {
-                rigidbody.AddForce(Vector2.right *25f);
+                rigidbody.AddForce(Vector2.right * 25f);
             }
             else
             {
-                rigidbody.AddForce(Vector2.right *14f);
+                rigidbody.AddForce(Vector2.right * 14f);
             }
         }
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if(jumpsLeft > 0)
+
+            if (Input.GetKey(KeyCode.RightArrow))
             {
-                jumpsLeft--;
+                Debug.Log("right1");
+                RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, transform.right, 0.6f);
 
-                if(highJumpTimer > 0)
+
+                for (int i = 0; i < hits.Length; i++)
                 {
-                    rigidbody.AddForce(Vector2.up * 20f, ForceMode2D.Impulse);
-                }
-                else
-                {
-                    rigidbody.AddForce(Vector2.up * 10f, ForceMode2D.Impulse);
+                    RaycastHit2D hit = hits[i];
+
+                    if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+                    {
+                        rigidbody.AddForce((Vector2.left + Vector2.up) * 15f, ForceMode2D.Impulse);
+                        Debug.Log("right2");
+                    }
                 }
             }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                Debug.Log("left1");
+                RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, -transform.right, 0.6f);
+
+
+                for (int i = 0; i < hits.Length; i++)
+                {
+                    RaycastHit2D hit = hits[i];
+
+                    if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+                    {
+                        rigidbody.AddForce((Vector2.right + Vector2.up) * 15f, ForceMode2D.Impulse);
+                        Debug.Log("left2");
+                    }
+                }
+            }
+            else
+            {
+                if (jumpsLeft > 0)
+                {
+                    jumpsLeft--;
+
+                    if (highJumpTimer > 0)
+                    {
+                        rigidbody.AddForce(Vector2.up * 20f, ForceMode2D.Impulse);
+                    }
+                    else
+                    {
+                        rigidbody.AddForce(Vector2.up * 10f, ForceMode2D.Impulse);
+                    }
+                }
+            }
+
+
         }
 
 
-        if(Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.D))
         {
-            if(projectileTimer > 0)
+            if (projectileNum > 0)
             {
                 GameObject newProjectile = Instantiate(projectilePrefab);
                 newProjectile.transform.position = transform.position;
                 newProjectile.transform.rotation = transform.rotation;
+                newProjectile.GetComponent<Rigidbody2D>().velocity = transform.right * 10f;
+                projectileNum--;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (projectileNum > 0)
+            {
+                GameObject newProjectile = Instantiate(projectilePrefab);
+                newProjectile.transform.position = transform.position;
+                newProjectile.transform.rotation = transform.rotation;
+                newProjectile.GetComponent<Rigidbody2D>().velocity = transform.right * -10f;
+                newProjectile.GetComponent<SpriteRenderer>().flipX = true;
+                projectileNum--;
             }
         }
 
         // use delta time for frame rate independence
-        if(highJumpTimer > 0)
+        if (highJumpTimer > 0)
         {
             highJumpTimer = highJumpTimer - Time.deltaTime;
         }
-        if(speedTimer > 0)
+        if (speedTimer > 0)
         {
             speedTimer = speedTimer - Time.deltaTime;
-        }
-        if(projectileTimer > 0)
-        {
-            projectileTimer = projectileTimer - Time.deltaTime;
         }
     }
 
     void OnCollisionStay2D(Collision2D other)
     {
         // check that we collided with ground
-        if(other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
         {
             RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, -transform.up, 0.7f);
             // Debug.DrawRay(transform.position, -transform.up *0.7f); // Vizualize RayCast
-            
-            for(int i=0; i < hits.Length; i++)
+
+            for (int i = 0; i < hits.Length; i++)
             {
                 RaycastHit2D hit = hits[i];
 
-                if(hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+                if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
                 {
                     jumpsLeft = 1;
                 }
@@ -113,24 +168,31 @@ public class PlayerController : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        if(other.gameObject.GetComponent<HighJumpGem>())
+        if (other.gameObject.GetComponent<HighJumpGem>())
         {
             highJumpTimer = 10f;
         }
 
-        if(other.gameObject.GetComponent<SpeedGem>())
+        if (other.gameObject.GetComponent<SpeedGem>())
         {
             speedTimer = 10f;
         }
 
-        if(other.gameObject.GetComponent<ProjectileStar>())
+        if (other.gameObject.GetComponent<ProjectileStar>())
         {
-            projectileTimer = 15f;
+            projectileNum += projectileIncrement;
         }
 
-        // if(other.gameObject.GetComponent<Door>())
-        // {
-        //     transform.position = new Vector3(-8.2f,-1f,0f);
-        // }
+        if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    private GUIStyle guiStyle = new GUIStyle();
+    void OnGUI()
+    {
+        guiStyle.fontSize = 35;
+        GUI.Label(new Rect(10, 900, 100, 20), "Arrows Left: " + projectileNum.ToString(), guiStyle);
     }
 }
