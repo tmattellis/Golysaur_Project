2e61857bb4b1a284a4e8d87a3f2d8863b3bb8ec2
zack-kaplan <zack.kaplan@wustl.edu>
2020-03-01 14:53:35 -0600

    Added destructible terrain and new scene.

---
+++ Assets/Scripts/Destructible.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Projectile>())
+        {
+            Destroy(gameObject);
+        }
+    }
+}
