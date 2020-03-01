a43343b2fbf4d175f08c4a74359dc66a62e2dd89
jackp9711 <jackp9711@gmail.com>
2020-03-01 13:44:25 -0600

    fixed enemy hitboxes and shooting mechanics

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    Rigidbody2D rigidbody;
+    
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
-        rigidbody.velocity = transform.right * 10f;
+
     }
 
     void OnCollisionEnter2D(Collision2D collision)
