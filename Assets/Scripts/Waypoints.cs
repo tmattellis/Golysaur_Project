46ca105d6668a1c0c9a45267d9452c92b813bac4
Yuxuan Ding <54516574+SybariteDDD@users.noreply.github.com>
2020-04-11 17:16:33 -0500

    Map1 complete
    Map1  Yuxuan Ding

---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float waitSeconds = 0;
+    public float speedOut = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
